377bab0d3a6862acb3c21b421f829b6ca09e0bb5
YaoYue <938778575@qq.com>
2022-10-23 21:04:20 +0800

    基本组件构建

---
+++ components/comment/index.js
@@ -0,0 +1,28 @@
+// components/comment/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    data: {
+      type: Object,
+      value: {
+        is_stared: true
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
