2814cb8a41e114a3816d84a5a4009f0edd9909a5
YaoYue <938778575@qq.com>
2022-10-27 17:48:45 +0800

    UI调整

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,96 +1,29 @@
 // pages/home/index.js
-import {
-  initTabActive
-} from "../../utils/index";
-import {
-  getToken
-} from "../../utils/action";
+import { initTabActive } from '../../utils/index'
+import { getToken } from '../../utils/action'
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     isLogin: false,
-    searchValue: '',
-    reArray: [{
-        name: '全部任务',
-        count: 60,
-        some: true,
-        style: 'background:#7c3aed',
-        url: '../../images/date.png'
-      },
-      {
-        name: '已过期',
-        count: 1,
-        some: false,
-        style: 'background: rgb(231, 125, 125);',
-        url: '../../images/date.png'
-      },
-      {
-        name: '今日',
-        count: 9,
-        some: false,
-        style: 'background: #4f46e5',
-        url: '../../images/date.png'
-      },
-      {
-        name: '已完成任务',
-        count: 169,
-        some: true,
-        style: 'background:#0891b2',
-        url: '../../images/date.png'
-      },
-    ],
-    selectList: [],
-    todoList: [{
-        icon: '',
-        content: '学习',
-        time: '09:00',
-        level: 0,
-        value: 'id0'
-      },
-      {
-        icon: '',
-        content: '吃早饭',
-        time: '10:00',
-        level: 0,
-        value: 'id1'
-      },
-      {
-        icon: '',
-        content: '绘制油画',
-        time: '11:00',
-        level: 1,
-        value: 'id2'
-      },
-      {
-        icon: '',
-        content: '中午睡觉',
-        time: '12:00',
-        level: 2,
-        value: 'id3'
-      },
-    ],
-    current: [],
     hasLogin: !!getToken(),
     avatar_url: wx.getStorageSync('avatar_url'),
     username: wx.getStorageSync('username'),
+    weather_default: 'http://192.168.1.166:7001/public/weather/night_bg.png'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    console.log('页面加载');
+    console.log('页面加载')
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
@@ -107,40 +40,28 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  },
-  onSearch() {
-
-  },
+  onShareAppMessage() {},
+  onSearch() {},
   onCancel() {
     this.setData({
       searchValue: ''
@@ -148,9 +69,9 @@ Page({
   },
   onChange(event) {
     this.setData({
-      selectList: event.detail,
-    });
-    const arr = this.data.todoList.map(i => event.detail.includes(i.value))
+      selectList: event.detail
+    })
+    const arr = this.data.todoList.map((i) => event.detail.includes(i.value))
     this.setData({
       current: arr
     })
